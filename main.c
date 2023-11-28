b0dfe965b20e005f7665e097dbcc97f701b04463
Rachael Rosaline <rachaelrosaline@openmail.cc>
2023-11-28 00:46:21 -0600

    remove warnings with vanilla openssl

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     BIO *o = BIO_new_fp(stdout, BIO_NOCLOSE);
     SSL_CTX *c;
-    if((c = SSL_CTX_new(TLSv1_2_client_method())) == NULL) {
+    if((c = SSL_CTX_new(TLS_client_method())) == NULL) {
         BIO_printf(o, "SSL_CTX_new error\n");
         return 1;
     }
